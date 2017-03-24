ece1b887beaa02bb508ea7d8269e7bd5b3d39de8
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2017-03-25 03:05:20 +0800

    merge with custom and standard in date display working

--- index.php
+++ index.php
@@ -38,41 +38,49 @@
       </div>
       <div id="home-date">
 
-
-
         <h3>Choose Date</h3>
 
-
         <?php
-
           require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-load.php");
-
+          $version = 2;
+          require_once("includes/Schedule.php");
           $date = array();
-
           $partner_id = $_GET['business'];
           $dateatthemo = date('H:i');
-
-        $newcon = new wpdb('dbo639369002','1qazxsw2!QAZXSW@','db639369002','db639369002.db.1and1.com');
-        $rows = $newcon->get_results("SELECT * FROM wp_bpc_appointment_settings WHERE partner_id = $partner_id && close = 'no'");
+          use App\Schedule;
 
 
-          foreach ($rows as $obj) :
+          /**
+           * Execute version 2
+           */
+          if($version == 2) {
+            $newcon         = new wpdb('dbo639369002', '1qazxsw2!QAZXSW@', 'db639369002', 'db639369002.db.1and1.com');
+            $resultCustom   = $newcon->get_results("SELECT * FROM wp_bpc_appointment_settings WHERE partner_id = $partner_id", ARRAY_A);
+            $resultStandard = $newcon->get_results("SELECT * FROM wp_bpc_appointment_setting_standard WHERE partner_id = $partner_id", ARRAY_A);
+            $schedule       = new Schedule($resultStandard, $resultCustom);
+            //            $schedule->print_r_pre($resultCustom, "custom");
+            //            $schedule->print_r_pre($resultStandard, "standard");
+            echo '<input type="hidden" id="datetofetch" name="datetofetch" value=' . $schedule->getFinalDatesFlattenToJson() . ' />';
+          }
+
+          /**
+           * Execute version 1
+           */
+          elseif($version==1) {
+            $newcon = new wpdb('dbo639369002','1qazxsw2!QAZXSW@','db639369002','db639369002.db.1and1.com');
+            $rows = $newcon->get_results("SELECT * FROM wp_bpc_appointment_settings WHERE partner_id = $partner_id && close = 'no'");
+            foreach ($rows as $obj) :
               if((strtotime($dateatthemo) < strtotime($obj->open_to)) and (strtotime($obj->date) == strtotime(date("Y-m-d"))) or (strtotime($obj->date) > strtotime(date("Y-m-d")))) {
-                  $originalDate = $obj->date;
-                  $newDate = date("Y-n-j", strtotime($originalDate));
-                  $newDate1 = explode("-", $newDate);
-                  $newDate2 = $newDate1[2].'-'.$newDate1[1].'-'.$newDate1[0];
-                  $date[] = $newDate2;
+                $originalDate = $obj->date;
+                $newDate = date("Y-n-j", strtotime($originalDate));
+                $newDate1 = explode("-", $newDate);
+                $newDate2 = $newDate1[2].'-'.$newDate1[1].'-'.$newDate1[0];
+                $date[] = $newDate2;
               }
-          endforeach;
-
-          $datetofetch = json_encode($date);
-
-
-
-
-
-          echo '<input type="hidden" id="datetofetch" name="datetofetch" value='.$datetofetch.' />';
+            endforeach;
+            $datetofetch = json_encode($date);
+            echo '<input type="hidden" id="datetofetch" name="datetofetch" value='.$datetofetch.' />';
+          }
 
         ?>        
         <div id="datepicker"></div>
